31c5432d148cf5c245ece9a0034f112b8a4fcf3f
tunim73 <antonio.netto973@gmail.com>
2023-04-03 08:16:41 -0300

    Fabrica de Carros

---
+++ src/FabricaDeCarros/Main.java
@@ -0,0 +1,32 @@
+package FabricaDeCarros;
+
+import java.util.*;
+
+public class Main{
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		int custoFabrica = scan.nextInt();
+		int porcentagemDistribuidor = scan.nextInt();
+		int PercentualImpostos = scan.nextInt();
+
+		int custoConsumidor;
+		int Distribuidor;
+		int ValorImpostos;
+
+
+		Distribuidor = (custoFabrica * porcentagemDistribuidor) / 100;
+		ValorImpostos = ( custoFabrica * PercentualImpostos ) /100;
+		
+		custoConsumidor = custoFabrica + Distribuidor + ValorImpostos; 
+
+		System.out.println("Valor do carro: R$ "+ custoConsumidor );
+		
+		
+		
+	}
+}
+
+
+
